e6646566e3628eed216f99a0c6dd578ff7e0e848
Hass <hassmroueh@gmail.com>
2024-05-06 22:14:05 -0400

    react router nested routes code along complete

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+
+import App from "./App"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -6,25 +8,34 @@ import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
+    path:'/',
+    element:<App />,
+    errorElement : <ErrorPage />,
+    children:[
+   
+  {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorPage />
+children : [
+  {
+    path: "/profile/:id",
+    element: <UserProfile />,
+    
+  }
+]
   }, 
   {
     path: "/about",
     element: <About />,
-    errorElement: <ErrorPage />
+ 
   },
   {
     path: "/login",
     element: <Login />,
-    errorElement: <ErrorPage />
+    
   },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
-  }
+   ]
+}
 ];
 
 export default routes;
